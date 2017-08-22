1f0d2504f86e9c794d958bf3f3f21c078109c938
KOF <ilikekof@126.com>
2017-08-22 15:54:32 +0800

    Added CrimeFragment class and fragment_crime layout

--- app/src/main/java/com/example/kof/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/kof/criminalintent/CrimeActivity.java
@@ -1,14 +1,29 @@
 package com.example.kof.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class CrimeActivity extends FragmentActivity {
+    private static final String TAG = "CrimeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+            Log.d(TAG, "onCreate: Created New fragment!");
+        }
+
+        Log.d(TAG, "onCreate: Executed onCreate Function!");
     }
 }
